d53d9217841bd1ff0cfdacbfc4d0ac55fcd55247
Lucas Vasques <lucasvc1996@gmail.com>
2024-02-24 20:54:04 -0300

    wip: user create account

--- spec/system/user/user_create_account_spec.rb
+++ spec/system/user/user_create_account_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 describe 'User creates an account' do
   it 'from the home page' do
+    visit root_path
+    click_on 'Create Account'
+    fill_in 'Name', with: 'Lucas Vasques'
+    fill_in 'E-mail', with: 'lucas@email.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Create Account'
+
+    expect(User.count).to eq 1
+    expect(page).to have_content 'Account created'
   end
 end
