b9d49bc54c965e7badc68427c58bf185627cd798
Lucas Vasques <lucasvc1996@gmail.com>
2024-02-28 11:47:24 -0300

    Added sign up and log in

--- spec/system/user/user_create_account_spec.rb
+++ spec/system/user/user_create_account_spec.rb
@@ -5,11 +5,14 @@ describe 'User creates an account' do
     visit root_path
     click_on 'Create Account'
     fill_in 'Name', with: 'Lucas Vasques'
-    fill_in 'E-mail', with: 'lucas@email.com'
+    fill_in 'Email', with: 'lucas@email.com'
     fill_in 'Password', with: 'password'
-    click_on 'Create Account'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
 
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(page).to have_content 'lucas@email.com'
+    expect(page).to have_button 'Sair'
     expect(User.count).to eq 1
-    expect(page).to have_content 'Account created'
   end
 end
